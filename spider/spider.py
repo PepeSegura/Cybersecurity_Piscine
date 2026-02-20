7a7512308f95180e2d053e9a5b14a99268f1513e
Pepe <pepeseguracarreno@gmail.com>
2026-02-20 13:45:52 +0100

    tests with ThreadPoolExecutor

--- spider/spider.py
+++ spider/spider.py
@@ -32,8 +32,21 @@ def parser():
     return parser.parse_args()
 
 
-def get_html(url) -> str :
-    return urllib.request.urlopen(url).read().decode('utf-8', errors='replace')
+def get_html() -> str :
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+    url = parsed_args.URL
+    try:
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            return response.read().decode('utf-8', errors='replace')
+    except Exception as e:
+        print(f"Error al acceder a {url}: {e}")
+        return ""
+
 
 def find_img_tags(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -41,15 +54,42 @@ def find_img_tags(html):
         img_url = img_tag.get('src')
         print("img_url: ", img_url)
 
+
 def find_a_tags(html):
     soup = BeautifulSoup(html, 'html.parser')
     for a_tag in soup.find_all('a'):
         url = a_tag.get('href')
-        print("next_url: ", url)
+        if url:
+            print("next_url: ", url)
+        else:
+            print("a:", a_tag)
+
+parsed_args = None
 
 if __name__ == '__main__':
     parsed_args = parser()
     print(parsed_args)
-    html = get_html(parsed_args.URL)
+    html = get_html()
     find_img_tags(html)
     find_a_tags(html)
+
+# from concurrent.futures import ThreadPoolExecutor
+# import time
+
+# def download_img(url_img):
+#     print(f"Iniciando descarga de: {url_img}")
+#     time.sleep(1)
+#     return f"OK: {url_img}"
+
+# url_lst = ["img1.png", "img2.jpg", "img3.gif"]
+
+# with ThreadPoolExecutor(max_workers=10) as executor:
+#     # .map() send each URL from the list to the function 'download_img()'
+#     results = executor.map(download_img, url_lst)
+
+# # 'results' generator with the result from each function 'dowload_img()'
+# i = 0
+# for r in results:
+#     print("res: ", r)
+#     i += 1
+# print("size: ", i)
